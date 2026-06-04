b2a40adc2bd16bf5a9dc2dc39a010fdba679daef
meng hor <piseth125125@gmail.com>
2026-06-04 12:16:03 +0700

    fix css

--- src/layouts/frontentLayout.js
+++ src/layouts/frontentLayout.js
@@ -5,13 +5,34 @@ import Footer from '../components/frontend/Footer';
 
 const FrontendLayout = () => {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+    <div style={{ 
+      display: 'flex', 
+      flexDirection: 'column', 
+      height: '100vh',        // បង្ខំកម្ពស់ឱ្យពេញអេក្រង់
+      overflow: 'hidden'      // ដក Scrollbar ចេញពីទំព័រទាំងមូល
+    }}>
       <Header />
-      {/* Added pt-5 (padding-top) or style padding to clear the fixed navbar */}
-      <main style={{ flex: 1, paddingTop: '75px' }}> 
+      
+      {/* កែសម្រួល main ឱ្យមាន Scrollbar លាក់ */}
+      <main style={{ 
+        flex: 1, 
+        paddingTop: '75px', 
+        overflowY: 'auto',      // ឱ្យវា Scroll នៅខាងក្នុង Main
+        msOverflowStyle: 'none', // សម្រាប់ IE និង Edge
+        scrollbarWidth: 'none'   // សម្រាប់ Firefox
+      }}> 
+        {/* លាក់ Scrollbar សម្រាប់ Chrome, Safari */}
+        <style>
+          {`
+            main::-webkit-scrollbar {
+              display: none;
+            }
+          `}
+        </style>
+        
         <Outlet />
+        <Footer />
       </main>
-      <Footer />
     </div>
   );
 };
